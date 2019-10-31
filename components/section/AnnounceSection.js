da46cd8f28a76a87f5e7f80c9d3cc8124ce69b5b
srosset81 <srosset81@gmail.com>
2019-10-31 09:39:55 +0100

    Update next event

--- components/section/AnnounceSection.js
+++ components/section/AnnounceSection.js
@@ -5,10 +5,10 @@ import { Text } from '../text';
 const AnnounceSection = () => (
   <Div bg="lightOrange" p="25px" align="center">
     <Text fontSize={{ xs: '1.3em', sm: '1.5em' }} m="0 0 10px">
-      Prochaine journée de Constellations Familiales le 13 octobre à Paris
+      Prochaine journée de Constellations Familiales le 8 décembre à Paris
     </Text>
     <br />
-    <a href="https://www.facebook.com/events/2512039039016477/" target="_blank" style={{ textDecoration: 'none' }}>
+    <a href="https://www.facebook.com/events/475880413137212/" target="_blank" style={{ textDecoration: 'none' }}>
       <Text italic color="black">
         Plus d'informations ici
       </Text>
