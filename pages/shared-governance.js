dbe20cd34887a29751dc44a0d0b95874a85dca66
srosset81 <srosset81@gmail.com>
2022-09-14 15:56:01 +0200

    Nouvel air site initial commit

---
+++ pages/shared-governance.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import Head from 'next/head';
+import { Layout } from 'components/app';
+import { SideImage } from 'components/media';
+import { LeadP, P, SubTitle } from 'components/text';
+import {
+  TopSection,
+  FooterSection,
+  TestimonySection,
+  ContentSection,
+  QuoteSection
+} from 'components/section';
+import { Trans } from '@lingui/macro';
+import ContactUsSection from "../components/section/ContactUsSection";
+import InformationSection from "../components/section/InformationSection";
+
+const SharedGovernancePage = () => (
+  <Layout>
+    <Head>
+      <title>Gouvernance partagée à Compiègne et à Paris - Nouvel air</title>
+    </Head>
+    <TopSection image="gouvernance-partagee.jpg">
+      <Trans id="shared-governance.title">Gouvernance partagée</Trans>
+    </TopSection>
+    <ContentSection>
+      <LeadP>
+        A Nouvel air, nous explorons les méthodes et postures de la Gouvernance partagée depuis bientôt 5 ans, avec
+        comme source d'inspiration principale l'Université du Nous. Nous croyons qu'il s'agit d'un élément-clé pour
+        redonner du souffle à une organisation.
+      </LeadP>
+      <SubTitle>Qu'est-ce que la Gouvernance partagée ?</SubTitle>
+      <P>
+        Le terme "Gouvernance partagée" a été popularisé en francophonie grâce au travail sans relâche de l'<a href="https://universite-du-nous.org/">Université
+        du Nous</a>, qui a fait découvrir à des milliers de personnes une autre manière de faire ensemble au travers
+        d'ateliers, de conférences mais aussi d'un grand MOOC organisé en partenariat avec le mouvement Colibris.
+      </P>
+      <P>
+        La Gouvernance partagée est constituée d'un certain nombre de méthodes, issues pour la plupart de la <a href="https://fr.wikipedia.org/wiki/Sociocratie">sociocratie</a> et de l'<a href="https://fr.wikipedia.org/wiki/Holacratie">holocratie</a>. Appliquées correctement, ces méthodes ont surtout l'avantage d'inviter les participants à
+        adopter un changement de posture par rapport à ce à quoi ils sont habitués. Car évidemment, aucun changement
+        extérieur ne peut être durable sans un changement intérieur.
+      </P>
+      <SideImage align="right" src="/static/images/fusee.jpg" />
+      <P>
+        Contrairement à ce que le terme peut laisser penser, la Gouvernance partagée ne se focalise pas uniquement sur
+        le fait d'amener plus d'horizontalité, mais elle valorise aussi la verticalité. Il ne s'agit pas toujours
+        prendre toutes les décisions en communs, mais aussi de décider lorsqu'il est utile qu'il y ait des responsables.
+      </P>
+      <SubTitle>Pourquoi la gouvernance partagée ?</SubTitle>
+      <ul>
+        <li>
+          <P>
+            Permettre une plus grande implication des membres, quel que soit leur statut (cadre, employé, bénévole...)
+            en leur permettant de trouver leur juste place dans l'organisation.
+          </P>
+        </li>
+        <li>
+          <P>
+            Mettre en valeur la raison d'être de l'organisation, afin qu'elle soit en phare dans les actions posées par
+            chacun et chacun au quotidien.
+          </P>
+        </li>
+        <li>
+          <P>
+            Proposer des réunions plus efficaces et satisfaisantes pour tout le monde, grâce à des cadres clairs
+            permettant à tout le monde de s'exprimer et de contribuer.
+          </P>
+        </li>
+        <li>
+          <P>
+            Amener plus d'intelligence collective dans les décisions, en particulier dans les décisions ayant un
+            impact sur la stratégie à moyen et long termes.
+          </P>
+        </li>
+        <li>
+          <P>
+            Faire vivre à tous une expérience humaine intense et riche de sens, avec à la clé une nouvelle manière
+            de faire ensemble.
+          </P>
+        </li>
+      </ul>
+    </ContentSection>
+    <InformationSection image="coeur-poumons-2.jpg">
+      <strong>L'analogie coeur-poumons</strong> : dans le corps humain, il n'y a pas de supériorité entre les organes.
+      Les poumons apportent de l'oxygène, tandis que le coeur impulse le sang à travers tout le corps. Chacun a une
+      fonction bien définie, avec un périmètre clair. Si le sang déborde dans les poumons, c'est la mort. En Gouvernance
+      partagée, c'est pareil: chaque rôle a une fonction claire, avec un périmètre. Il faut que les deux fonctionnent
+      ensemble, mais il n'est pas nécessaire qu'ils décident ensemble.
+    </InformationSection>
+    <QuoteSection author="Université du Nous" color="black" bg="tertiaryLight">
+      Si  nous  imaginons  notre  projet  comme  un  grand  jeu  collectif  coopératif,  comme  dans  n’importe quel  jeu,  pour  que  ça  fonctionne,  il  faut  des  règles.  Ce  sera  l’ensemble  des  règles  relationnelles  et organisationnelles qui permettront de répondre aux questions que posent tout projet collectif
+    </QuoteSection>
+    <ContentSection>
+      <SubTitle>L'approche Nouvel Air</SubTitle>
+      <P>
+        A Nouvel air nous proposons:
+      </P>
+      <ul>
+        <li>
+          <P>
+            Des expériences sur 3 jours pour découvrir par la pratique la Gouvernance partagée. Ces expériences étant
+            ouvertes à tous, elles sont l'occasion de rencontrer des personnes avec des besoins et des expériences
+            très différentes et de collaborer sur l'élaboration d'un défi choisi collectivement.
+          </P>
+        </li>
+        <li>
+          <P>
+            Un accompagnement spécifique au sein des organisations (existantes ou en cours de formation) en amenant
+            les ingrédients de la Gouvernance partagée dont elles ont besoin pour mieux fonctionner.
+          </P>
+        </li>
+      </ul>
+      <P style={{ clear: 'both' }}></P>
+    </ContentSection>
+    <TestimonySection id="1519285" />
+    <ContactUsSection />
+    <FooterSection />
+  </Layout>
+);
+
+export default SharedGovernancePage;
