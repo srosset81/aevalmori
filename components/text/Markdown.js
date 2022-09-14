929012cf0086217ea868dc0358a9141f449402d6
srosset81 <srosset81@gmail.com>
2022-09-14 13:13:10 +0200

    Fix lists

--- components/text/Markdown.js
+++ components/text/Markdown.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import MarkdownToJSX from 'markdown-to-jsx';
 import P from "./P";
-import { Ul, Li } from "../layout";
 
 const Markdown = (props) => (
   <MarkdownToJSX
     options={{
       overrides: {
         p: P,
-        ul: Ul,
-        li: Li
+        li: props => (<li><P {...props}></P></li>)
       },
     }}
     {...props}
